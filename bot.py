92d4a704e0c55f2629ac8d02d4f9ead69c028189
Grace Hadiyanto <gracehadiyanto@gmail.com>
2023-03-17 00:31:58 -0700

    Added error handling/logging, static speak features, and updated .gitignore

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
-
+import sys
+import random
 import discord
 from dotenv import load_dotenv
 
@@ -11,6 +12,10 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+
+"""
+Whenever the connection is ready
+"""
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, id=int(GUILD))
@@ -19,5 +24,54 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+
+"""
+Whenever a message is sent
+"""
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    text = message.content.lower()
+
+    ambers_vocabulary = [
+        'Miaoooo~',
+        'Mao',
+        'Meooow',
+        'meow,.. meow,.. meow..'
+    ]
+
+    speak_commands = {"amber speak", "amber say something", "amber talk"}
+
+    if text in speak_commands:
+        response = random.choice(ambers_vocabulary)
+        await message.channel.send(response)
+
+
+"""
+Whenever a new member joins the guild
+"""
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel_send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+
+"""
+Log all errors to a file instead
+"""
+@client.event
+async def on_error(event, *args, **kwargs):
+    e_info = sys.exc_info()
+    with open('err.log', 'a') as f:
+        f.write(f'{e_info}\n')
+        print(f'**ERROR** {e_info[0]}: {e_info[1]}. Added a new entry to err.log')
+    f.close()
+
+
 
 client.run(TOKEN)

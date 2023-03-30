9f97ca4c40ddfaec7ecb2b3bb9b0bb91139cda03
Grace Hadiyanto <gracehadiyanto@gmail.com>
2023-03-29 23:16:00 -0700

    Added features for Last Epoch (hardcore mode) death counter

--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@ import os
 import sys
 import random
 import discord
+import json
 from dotenv import load_dotenv
 from discord.ext import commands
 from lib import random_cfg_deriver
+from lib import lastepoch_helper
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -95,5 +97,53 @@ async def bobarec(ctx):
     response = random_cfg_deriver.process_grammar('lib/teatime.cfg')
     await ctx.send(response)
 
+"""
+[Last Epoch] Show leaderboard for death counters
+"""
+@bot.command(name='deaths', help='Show the current leaderboard for Last Epoch (hardcore mode) deaths')
+async def deaths(ctx):
+    response = """```\n
+*-------------------------------*
+|    Death Count Leaderboard    |
+*-------------------------------*
+
+"""
+    with open('lib/lastepoch_deathcounts.json', 'r') as f:
+        death_counts = json.load(f, parse_int=lambda x: int(x))
+    f.close()
+
+    sorted_counts = sorted(death_counts.items(), key=lambda kv: (kv[1][1], kv[0]), reverse=True)
+    for count in sorted_counts:
+        response += f'{count[1][0]}: {count[1][1]}\n'
+    response += "\n```"
+    await ctx.send(response)
+
+"""
+[Last Epoch] Increment death count
+"""
+@bot.command(name='idied', help='Add 1 to your Last Epoch death counter')
+async def idied(ctx):
+    with open('lib/lastepoch_deathcounts.json', 'r') as f:
+        death_counts = json.load(f, parse_int=lambda x: int(x))
+    f.close()
+
+    mykey = str(ctx.message.author.id)
+    myname = str(ctx.message.author.name)
+
+    if mykey in death_counts:
+        val = death_counts[mykey][1]
+        death_counts[mykey] = [myname, val + 1]
+    else:
+        death_counts[mykey] = [myname, 1]
+
+    with open('lib/lastepoch_deathcounts.json', 'w') as f:
+        json.dump(death_counts, f, indent=4)
+    f.close()
+
+    response = random.choice(lastepoch_helper.rip_messages)
+    response += "I incremented your death count by 1, you can thank me with some treats :3\n"
+    response += f'```\n{death_counts[mykey][0]}: {death_counts[mykey][1]}\n```'
+
+    await ctx.send(response)
 
 bot.run(TOKEN)
---
+++ lib/lastepoch_helper.py
@@ -0,0 +1,7 @@
+
+
+rip_messages = [
+    "WOMP WOMP WOMPPPPPPP! Nice try, but try harder next time T_T\n",
+    "RIP, is it time for TFT?\n",
+    "Oops! Lets try to avoid that next time\n",
+]
